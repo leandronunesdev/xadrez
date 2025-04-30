04f149551ad4fec280f33602a64174f537f0220d
Leandro <nunes.pessoal@gmail.com>
2025-04-30 19:48:48 -0300

    implementando as restricoes de movimento

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -8,22 +8,36 @@ try
 
   while (!partida.terminada)
   {
-    Console.Clear();
-    Tela.imprimirTabuleiro(partida.tab);
+    try
+    {
 
-    Console.WriteLine();
-    Console.Write("Origem: ");
-    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+      Console.Clear();
+      Tela.imprimirTabuleiro(partida.tab);
+      Console.WriteLine();
+      Console.WriteLine($"Turno: {partida.turno}");
+      Console.WriteLine($"Aguardando jogada: {partida.jogadorAtual}");
 
-    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+      Console.WriteLine();
+      Console.Write("Origem: ");
+      Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+      partida.validarPosicaoDeOrigem(origem);
 
-    Console.Clear();
-    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+      bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-    Console.Write("Destino: ");
-    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+      Console.Clear();
+      Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
-    partida.executaMovimento(origem, destino);
+      Console.Write("Destino: ");
+      Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+      partida.validarPosicaoDeDestino(origem, destino);
+
+      partida.realizaJogada(origem, destino);
+    }
+    catch (TabuleiroException e)
+    {
+      Console.WriteLine(e.Message);
+      Console.ReadLine();
+    }
   }
 
 }
